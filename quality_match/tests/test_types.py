d500cd29d8827a71d0e4c11cecff739189e376af
fahimeh <beigifahimeh73@gmail.com>
2023-03-30 14:38:06 +0200

    save result

--- quality_match/tests/test_types.py
+++ quality_match/tests/test_types.py
@@ -1,6 +1,6 @@
 import pytest
 
-from quality_match.types import from_bool
+from quality_match.types import from_bool, from_str
 
 
 @pytest.mark.parametrize("test_input,expected", [(True, True), (False, False)])
@@ -11,3 +11,10 @@ def test_from_bool(test_input, expected):
 def test_from_bool_raises():
     with pytest.raises(AssertionError):
         from_bool("no-a-valid-bool")
+
+def test_from_str():
+    assert from_str("hello") == "hello"
+
+def test_from_str_raises():
+    with pytest.raises(AssertionError):
+        from_str(0)
